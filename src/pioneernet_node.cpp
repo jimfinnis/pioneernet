b617ccb7b66bca221a4ce0d52537c649bebb5da9
Jim Finnis <jim.finnis@gmail.com>
2016-04-25 18:34:07 +0100

    Subscribes to a number of sonar topics

--- src/pioneernet_node.cpp
+++ src/pioneernet_node.cpp
@@ -7,10 +7,26 @@
 #include "backpropNoBiasHormone.h"
 
 #include "lightsensor_gazebo/LightSensor.h"
+#include "sensor_msgs/Range.h"
+
+
+#define NUM_SONARS 8
+
+void sonarCallback(const sensor_msgs::Range::ConstPtr& msg,int i){
+}
 
 int main(int argc,char *argv[]){
     ros::init(argc,argv,"pioneernet_node");
     ros::NodeHandle n;
+    
+    ros::Subscriber s[NUM_SONARS];
+    for(int i=0;i<NUM_SONARS;i++){
+        char buf[32];
+        sprintf(buf,"s%d",i);
+        s[i] = n.subscribe<sensor_msgs::Range>(buf,1000,
+                                 boost::bind(sonarCallback,_1,i));
+    }
+    
     
     ros::Rate rate(10);
     while(ros::ok()){
