ed82e54da1fc38d1b0d28cac07c714a6b6f0a475
Jim Finnis <jim.finnis@gmail.com>
2016-05-23 20:06:09 +0100

    Throttling on logpos.py

--- scripts/logpos.py
+++ scripts/logpos.py
@@ -3,13 +3,18 @@
 import rospy
 from gazebo_msgs.msg import ModelStates
 
-print "init"
 rospy.init_node('logpos',anonymous=True)
 
+# crude throttling
+msgidx=0
+
 def callback(data):
-    idx=data.name.index('robot_description')
-    pos = data.pose[idx].position
-    print "%f,%f" % (pos.x,pos.y)
+    global msgidx
+    if(msgidx%100==0):
+        idx=data.name.index('robot_description')
+        pos = data.pose[idx].position
+        print "%f,%f" % (pos.x,pos.y)
+    msgidx=msgidx+1
     
 rospy.Subscriber('gazebo/model_states',ModelStates,callback)
 
